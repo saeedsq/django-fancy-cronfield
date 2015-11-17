78113d66b20e7739c0c1ed379b55e775367d5790
Saeed Salehian <saeed.sq@gmail.com>
2015-11-18 01:02:58 +0330

    Issue #7 - In Python 2.6.9 AttributeError: 'TestLoader' object has no attribute 'discover' Checked for python version and used unittest2 in python 2.6

--- fancy_cronfield/tests/__init__.py
+++ fancy_cronfield/tests/__init__.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import sys
 from fancy_cronfield.utils.compat import DJANGO_1_5
 
 if DJANGO_1_5:
-    import unittest
+    if sys.version_info < (2, 7):
+        import unittest2 as unittest_module
+    else:
+        import unittest as unittest_module
 
     def suite():
-        loader = unittest.TestLoader()
+        loader = unittest_module.TestLoader()
         return loader.discover("fancy_cronfield.tests", pattern="*.py")
