56a164d5e047eac8cdf2c5132cf3e8f4eafea7e2
Saeed Salehian <saeed.sq@gmail.com>
2015-11-24 16:16:53 +0330

    Issue #9 Release an Alpha version to pypi

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ import os
 import fancy_cronfield
 
 CLASSIFIERS = [
-    'Development Status :: 2 - Pre-Alpha',
+    'Development Status :: 3 - Alpha',
     'Environment :: Web Environment',
     'Framework :: Django :: 1.5',
-    'Intended Audience :: Developers',
     'Operating System :: OS Independent',
-    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Intended Audience :: Developers',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development',
     'Topic :: Software Development :: Libraries :: Application Frameworks',
-    "Programming Language :: Python :: 2.6",
 ]
 
 INSTALL_REQUIREMENTS = [
